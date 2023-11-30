8d8415c102366ec7655325937bf49b26cf24a7f2
Gábor Görzsöny <gabor@gorzsony.com>
2023-11-30 13:36:47 +0100

    Added test

---
+++ broadcaster_test.go
@@ -0,0 +1,44 @@
+package wskit
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/websocket"
+	"github.com/stretchr/testify/assert"
+)
+
+type testData struct {
+	N int
+}
+
+func TestBroadcast(t *testing.T) {
+	ch := make(chan testData)
+	b := Broadcaster[testData](ch)
+	server := httptest.NewServer(b)
+	defer server.Close()
+
+	url := "ws" + server.URL[4:]
+
+	ws1, _, err := websocket.DefaultDialer.Dial(url, nil)
+	assert.NoError(t, err)
+	defer ws1.Close()
+
+	ws2, _, err := websocket.DefaultDialer.Dial(url, nil)
+	assert.NoError(t, err)
+	defer ws2.Close()
+
+	go func() {
+		for i := 1; i <= 5; i++ {
+			ch <- testData{N: i}
+		}
+	}()
+
+	for i := 1; i <= 5; i++ {
+		var result1, result2 testData
+		assert.NoError(t, ws1.ReadJSON(&result1))
+		assert.Equal(t, i, result1.N)
+		assert.NoError(t, ws2.ReadJSON(&result2))
+		assert.Equal(t, i, result2.N)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,4 +4,11 @@ go 1.21.1
 
 require github.com/gorilla/websocket v1.5.1
 
-require golang.org/x/net v0.19.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.1 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
