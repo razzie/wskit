e704506d8e521d9af9258b38cebcb53c978eceb9
Gábor Görzsöny <gabor@gorzsony.com>
2023-11-30 21:56:25 +0100

    Added timeout test

--- broadcaster_test.go
+++ broadcaster_test.go
@@ -1,8 +1,10 @@
 package wskit
 
 import (
+	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/stretchr/testify/assert"
@@ -12,8 +14,21 @@ type testData struct {
 	N int
 }
 
+type delayedBufferPool struct {
+	delay time.Duration
+}
+
+func (dbp delayedBufferPool) Get() any {
+	time.Sleep(dbp.delay)
+	return nil
+}
+
+func (delayedBufferPool) Put(any) {
+}
+
 func TestBroadcast(t *testing.T) {
-	ch := make(chan testData)
+	const numMessages = 5
+	ch := make(chan testData, numMessages)
 	defer close(ch)
 
 	b := Broadcaster(ch)
@@ -30,17 +45,43 @@ func TestBroadcast(t *testing.T) {
 	assert.NoError(t, err)
 	defer ws2.Close()
 
-	go func() {
-		for i := 1; i <= 5; i++ {
-			ch <- testData{N: i}
-		}
-	}()
+	for i := 1; i <= numMessages; i++ {
+		ch <- testData{N: i}
+	}
 
-	for i := 1; i <= 5; i++ {
+	for i := 1; i <= numMessages; i++ {
 		var result1, result2 testData
 		assert.NoError(t, ws1.ReadJSON(&result1))
 		assert.Equal(t, i, result1.N)
 		assert.NoError(t, ws2.ReadJSON(&result2))
 		assert.Equal(t, i, result2.N)
 	}
+}
+
+func TestTimeout(t *testing.T) {
+	ch := make(chan testData, 2)
+	defer close(ch)
+
+	upgrader := &websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+		WriteBufferPool: &delayedBufferPool{delay: 200 * time.Millisecond},
+	}
+
+	b := Broadcaster(ch, WithTimeout(100*time.Millisecond), WithUpgrader(upgrader))
+	server := httptest.NewServer(b)
+	defer server.Close()
+
+	url := "ws" + server.URL[4:]
+	ws, _, err := websocket.DefaultDialer.Dial(url, nil)
+	assert.NoError(t, err)
+	defer ws.Close()
+
+	ch <- testData{N: 1}
+	ch <- testData{N: 2}
+
+	var result testData
+	assert.NoError(t, ws.ReadJSON(&result))
+	assert.NotNil(t, ws.ReadJSON(&result))
 }
